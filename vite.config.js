fe689432f455f23922f77c4c2dd41fd2b951fed9
FThreef <FThreef.535@gmail.com>
2024-05-14 08:42:36 +0300

    11

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ToDoBD',
+  base: '/ToDoBD/registration',
   plugins: [react()],
 })
